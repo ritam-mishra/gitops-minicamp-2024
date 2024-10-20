a706109085d73e462dce1287d482254579605349
ritam-mishra <ritammishra2002@gmail.com>
2024-10-20 20:47:26 +0000

    add s3 backend, terraform apply added

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,1 +1,10 @@
-terraform {}
+terraform {
+  required_version = "~> 1.9.5"
+
+  backend "s3" {
+    bucket         = "gitops-2024-backend"
+    key            = "terraform/state"
+    region         = "us-east-1"  
+    dynamodb_table = "Gitops2024TerraformLocks"
+  }
+}
