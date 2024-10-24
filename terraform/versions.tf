151817f7c75ddad206098ca82f4cbe40bd09a1f1
ritam-mishra <ritammishra2002@gmail.com>
2024-10-24 05:35:53 +0000

    added check for grafana

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -4,7 +4,7 @@ terraform {
   backend "s3" {
     bucket         = "gitops-2024-backend"
     key            = "terraform/state"
-    region         = "us-east-1"  
+    region         = "us-east-1"
     dynamodb_table = "Gitops2024TerraformLocks"
   }
 }
